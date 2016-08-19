cd9044b64ff8bfa6d2a5de080abfbdc885d4bb89
Halimat Mercy Oseni <mehrceey04@gmail.com>
2016-08-19 07:42:40 +0100

    Test is working

--- tests.js
+++ tests.js
@@ -1,24 +1,47 @@
 'use strict'
-
+require('mocha');
 var chai = require('chai');
 var assert = chai.assert;
 
-var NotesApplication = require('./lib/notesapplication.js');
-var Note = require('./lib/note.js');
+var NotesApp = require('./lib/notesapplication.js');
+var Note = require('./lib/notes.js');
+
 
 describe("Note creation works properly", function() {
     it("assigns author based on the parameter supplied in the constructor", function() {
-        note = new Note("Hello world", "Chidiebere")
-        assert(note.author == "Chidiebere")
+        var note = new Note("Mercy", "Hello mickey");
+        assert(note.author == "Mercy", "author not defined")
     })
 })
 
-describe("Notes application increments number of notes as notes are added", function() {
-    it("increments the note list as notes are added", function() {
-        note = new Note("Hello world", "Chidiebere");
-        noteapp = new NotesApplication("Chidiebere");
-        assert(noteapp.notelist.length == 0)
-        noteapp.addNote(note)
-        assert(noteapp.notelist.length == 1)
+describe("Note App creation works properly", function() {
+
+    it("checks notelist created", function(){
+        var myNoteApp = new NotesApp();
+        assert(myNoteApp.notelist.length == 0, "notelist is not equal to empty list")
+    })
+    
+    it("Creates application", function(){
+        var myNoteApp = new NotesApp();
+        var note = new Note("Mercy", "Hello mickey");
+        myNoteApp.create(note)
+        assert(myNoteApp.notelist.length == 1, "notelist is  empty")
+    })
+    
+    it("it gets the note ID", function() {
+        var noteapp = new NotesApp("Chidiebere");
+        var note = new Note("Mercy", "Hello mickey");
+        noteapp.create(note)
+        var note_id = noteapp.get(0); 
+        var getIdError = "'No note with ID ' + note_id "
+        assert(noteapp.get(0)!== getIdError )
+    })
+     
+     it("it checks if note exists", function() {
+        var  note = new Note("Mickey Mouse", "Tom Jerry");
+        var  noteapp = new NotesApp("Chidiebere");
+        noteapp.create(note)
+        var noteError = "Note doesn't exist"
+        assert(noteapp.get(0) !== noteError);
     })
 })
